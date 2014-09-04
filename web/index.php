7613cf0f25beef25c914db45beeb3116fb398408
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-09-04 16:22:18 +0200

    Added a Twig view, still testing

--- web/index.php
+++ web/index.php
@@ -4,10 +4,25 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app['debug'] = true;
+
+// Register the Twig service provider
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
 $app->get('/hello/{name}', function($name) use ($app) {
     return 'Hello ' . $app->escape($name) . '!';
 })
 ->value('name', 'world');
+
+$app->get('/search/{name}', function ($name) use ($app) {
+    return $app['twig']->render('search.twig', array(
+        'title' => 'Search',
+        'name' => $name,
+    ));
+})
+->value('name', '');
 
 $app->get('/', function() use($app) {
 	return 'Home page!';

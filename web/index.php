4da9beaef487f7c12bef20a1aba4dae6d3f0298c
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-09-07 18:57:58 +0200

    Minimal integration of the ltNumbers module

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app['appName'] = 'ltWords';
 $app['debug'] = true;
 
 // Register the Twig service provider
@@ -11,21 +12,29 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-$app->get('/hello/{name}', function($name) use ($app) {
-    return 'Hello ' . $app->escape($name) . '!';
-})
-->value('name', 'world');
-
 $app->get('/search/{name}', function ($name) use ($app) {
+	$parameter = $name;
+	if ($parameter == '') {
+		$parameter = $app['request']->get('searchString');
+	}
+	$nameStr = $parameter;
+	if (is_numeric($nameStr)) {
+		$ltNumbers = new LtWords\LtNumbers\LtNumbers;
+		$nameStr = $ltNumbers->numberToText($parameter);
+	}
+
     return $app['twig']->render('search.twig', array(
         'title' => 'Search',
-        'name' => $name,
+        'name' => $parameter,
+        'result' => $nameStr
     ));
 })
 ->value('name', '');
 
 $app->get('/', function() use($app) {
-	return 'Home page!';
+    return $app['twig']->render('index.twig', array(
+        'title' => $app['appName']
+    ));
 });
 
 $app->run();

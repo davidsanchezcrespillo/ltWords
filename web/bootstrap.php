bd5b52e8e8cfb703f5c785df2bc73529e803459a
David Sanchez Crespillo <davidsanchezcrespillo@gmail.com>
2014-10-09 14:55:11 +0200

    Added multi-language support in the user interface

---
+++ web/bootstrap.php
@@ -0,0 +1,39 @@
+<?php 
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['appName'] = 'ltWords';
+$app['debug'] = true;
+
+// Register the Twig service provider
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Register the session service provider
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+// Register the translation service provider
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'locale_fallback' => 'en',
+));
+
+// Add the YAML file loader to the translator
+$app['translator'] = $app->share(
+    $app->extend('translator', function ($translator, $app) {
+        $translator->addLoader('yaml', new Symfony\Component\Translation\Loader\YamlFileLoader());
+ 
+        return $translator;
+    })
+);
+
+// Register the Monolog service provider
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.name' => 'ltWords',
+    'monolog.logfile' => __DIR__.'/ltWords.log',
+    'monolog.level' => Monolog\Logger::DEBUG,
+));
+
+return $app;
--- web/index.php
+++ web/index.php
@@ -1,16 +1,39 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\Yaml\Parser;
 
-$app = new Silex\Application();
+$app = require __DIR__ . '/bootstrap.php';
 
-$app['appName'] = 'ltWords';
-$app['debug'] = true;
+// Get the default language from the session
+$lang = "en";
+if ($app['session']->get('current_language')) {
+    $lang = $app['session']->get('current_language');
+}
+$app['monolog']->addDebug("Language: $lang");
+ 
+foreach (glob(__DIR__ . '/locale/'. $lang . '/*.yml') as $locale) {
+    $app['monolog']->addDebug("Locale: $locale");
+    $app['translator']->addResource('yaml', $locale, $lang);
+}
 
-// Register the Twig service provider
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+// Set the current language
+$app['translator']->setLocale($lang);
+
+// Get the list of current languages from the config file
+$yamlParser = new Parser();
+$images = $yamlParser->parse(file_get_contents(__DIR__ . '/images.yml'));
+$app['images'] = $images;
+
+//$app['monolog']->addDebug("Languages: $languages");
+
+$app->get('/lang/{lang}', function($lang) use($app) {
+    // check if language exists
+    if (is_dir(__DIR__ . '/locale/' . $lang)) {
+        // save user selection in session
+        $app['session']->set('current_language', $lang);    
+    }
+    return $app->redirect($_SERVER['HTTP_REFERER']);
+});
 
 $app->get('/numbers/{name}', function ($name) use ($app) {
     $parameter = $name;
@@ -26,7 +49,8 @@ $app->get('/numbers/{name}', function ($name) use ($app) {
     return $app['twig']->render('numbers.twig', array(
         'title' => 'LTNumbers',
         'name' => $parameter,
-        'result' => $nameStr
+        'result' => $nameStr,
+        'images' => $app['images'],
     ));
 })
 ->value('name', '');
@@ -43,14 +67,23 @@ $app->get('/nouns/{name}', function ($name) use ($app) {
     return $app['twig']->render('nouns.twig', array(
         'title' => 'LTNouns',
         'name' => $parameter,
-        'result' => $nounDeclensions
+        'result' => $nounDeclensions,
+        'images' => $app['images'],
     ));
 })
 ->value('name', '');
 
+$app->get('/img/{name}', function ($name) use ($app) {
+    $fullName = __DIR__ . '/img/' . $name;
+    
+    $app['monolog']->addDebug("Image name: $fullName");
+
+    return $app->sendFile($fullName);
+});
+
 $app->get('/', function () use ($app) {
     return $app['twig']->render('index.twig', array(
-        'title' => $app['appName']
+        'title' => $app['appName'],
     ));
 });
 
